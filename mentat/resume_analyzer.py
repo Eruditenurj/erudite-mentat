712550720de8d1825193b9945d51d61198984885
SCP2022Rylan <scp2022rylan@gmail.com>
2021-08-25 20:12:20 -0400

    Finished issue-2

--- mentat/resume_analyzer.py
+++ mentat/resume_analyzer.py
@@ -1,21 +1,52 @@
 import sys
+import csv
 
 OPPURTUNITIES_FILENAME = "oppurtunities.csv"
 
+
+
 # extracts and returns a list of keywords from a pdf file
 def find_keywords(filename):
-    # load list of allowed_keywords
-    allowed_keywords = [] # get_allowed_keywords
-    # load and read pdf
+   
+	list_keywords = []
+	
+	# load list of allowed_keywords
+	allowed_keywords = get_allowed_keywords() # get_allowed_keywords
+	
+   # load and read pdf
+	pdffile = open(filename, 'r')
+	
+	all_words = pdffile.read()
+	
     # go through every word and check if it is in allowed_keywords list
     # if it is add it to the list of keywords to return
+	
+	for word in all_words:
+		for keyword in allowed_keywords:
+			if word == keyword:
+				list_keywords.append(word)
     # example return:
-    return ['Artificial Intelligence']
+	return list_keywords
 
 def get_allowed_keywords():
     # read the oppurtunities file get an unique set of all the keywords in all
     # of the rows
-    pass
+	
+	unique_keywords = []
+
+	with open ('opportunities.csv') as csvfile:
+		csv_reader = csv.reader(csvfile, delimiter=',',quotechar='"')
+		for row in csv_reader:
+			count = 0
+			temporary_keywords=[row[15]]
+			for x in temporary_keywords:
+				for y in unique_keywords:
+					if x == y:
+						continue
+				unique_keywords.append(x)
+			count = count + 1
+		
+	return unique_keywords
 
 if __name__ == "__main__":
     filename = sys.argv[1]
