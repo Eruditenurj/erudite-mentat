3f722497992d79492c591bd3b6b038270c9ef3e6
Saketh Dargula <sdargula@gmail.com>
2021-08-26 09:41:38 -0400

    added pdf library

--- mentat/resume_analyzer.py
+++ mentat/resume_analyzer.py
@@ -1,40 +1,39 @@
 import sys
 import csv
+import fitz
 
-OPPURTUNITIES_FILENAME = "oppurtunities.csv"
-
-
+OPPORTUNITIES_FILENAME = "opportunities.csv"
 
 # extracts and returns a list of keywords from a pdf file
 def find_keywords(filename):
-   
 	list_keywords = []
-	
+
 	# load list of allowed_keywords
-	allowed_keywords = get_allowed_keywords() # get_allowed_keywords
-	
-   # load and read pdf
-	pdffile = open(filename, 'r')
-	
-	all_words = pdffile.read()
-	
-    # go through every word and check if it is in allowed_keywords list
-    # if it is add it to the list of keywords to return
-	
-	for word in all_words:
+	allowed_keywords = get_allowed_keywords()
+	print("allowed_keywords", allowed_keywords)
+
+	# load and read pdf
+	doc = fitz.open(filename)
+	page = doc.load_page(0)
+	text = page.get_text()
+
+	# go through every word and check if it is in allowed_keywords list
+	# if it is add it to the list of keywords to return
+
+	for word in text:
 		for keyword in allowed_keywords:
 			if word == keyword:
 				list_keywords.append(word)
-    # example return:
+
 	return list_keywords
 
 def get_allowed_keywords():
-    # read the oppurtunities file get an unique set of all the keywords in all
-    # of the rows
-	
+	# read the oppurtunities file get an unique set of all the keywords in all
+	# of the rows
+
 	unique_keywords = []
 
-	with open ('opportunities.csv') as csvfile:
+	with open (OPPORTUNITIES_FILENAME) as csvfile:
 		csv_reader = csv.reader(csvfile, delimiter=',',quotechar='"')
 		for row in csv_reader:
 			count = 0
@@ -45,10 +44,10 @@ def get_allowed_keywords():
 						continue
 				unique_keywords.append(x)
 			count = count + 1
-		
+
 	return unique_keywords
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    keywords = find_keywords(filename)
-    print(keywords)
+	filename = sys.argv[1]
+	keywords = find_keywords(filename)
+	print(keywords)
