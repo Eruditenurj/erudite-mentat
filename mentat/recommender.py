c96d6a22601b435623156a9e4013aee105364bbd
Saketh Dargula <sdargula@gmail.com>
2021-09-08 17:57:51 -0400

    added examples and comments on how to use resume analyzer

--- mentat/recommender.py
+++ mentat/recommender.py
@@ -1,8 +1,19 @@
 import sys
+import resume_analyzer
 
 # calculates which opportunities most aligns with the given keywords
 # returns a list of opportunities
 def get_recommended_opportunities(keywords):
+    allowed_keywords = resume_analyzer.get_allowed_keywords()
+    # make count-vectorizer for resume
+
+    # make count-vectorizer for all opportunites
+    # opportunities = resume_analyzer.get_opportunities()
+    
+    # compute cosine similarity between resume and opportunities
+
+    # return the 5 opportunities with the most cosine similarity
+
     # example return:
     return [
         {
@@ -25,5 +36,5 @@ def get_recommended_opportunities(keywords):
 
 if __name__ == "__main__":
     keywords = sys.argv[1:]
-    oppurtunities = get_recommended_opportunities(keywords)
-    print(oppurtunities)
+    opportunities = get_recommended_opportunities(keywords)
+    print(opportunities)
